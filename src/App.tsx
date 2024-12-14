1795a643a4ce2f836886835b933361cd90750300
sai-oduri <saioduri95@gmail.com>
2024-12-14 15:35:31 +0530

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
 import { SelectedPage } from "@/shared/types";
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   )
 }
