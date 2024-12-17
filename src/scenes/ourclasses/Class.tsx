d77f13f035b542376a1dc2e028186837e969c68b
sai-oduri <saioduri95@gmail.com>
2024-12-17 13:50:22 +0530

    contact page

--- src/scenes/ourclasses/Class.tsx
+++ src/scenes/ourclasses/Class.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Class = ({ name, description, image }: Props) => {
 
-  const overlayStyles = `p-5 absolute z-30 flex flex-col h-[380px] w-[450px] items-center 
+  const overlayStyles = `p-5 absolute z-20 flex flex-col h-[380px] w-[450px] items-center 
   justify-center whitespace-normal bg-primary-500 text-center text-white opacity-0 
   transition duration-500 hover:opacity-90`
 
